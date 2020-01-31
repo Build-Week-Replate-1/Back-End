f1ae2400f2a8252ae71c24a36c1845eb675a19f0
jacobcalv <jacobcalvino@outlook.com>
2020-01-30 20:48:00 -0600

    .

--- knexfile.js
+++ knexfile.js
@@ -24,11 +24,6 @@ module.exports = {
         },
         seeds: {
             directory: './database/seeds'
-        },
-        pool:{
-            afterCreate:(conn, done) => {
-                conn.run('PRAGMA foreign_keys = ON', done)
-            }
         }
     }
 }
